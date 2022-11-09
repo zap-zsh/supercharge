67640bcbb7d3e8b960a7df37e67997e7c93a521e
Christian Chiarulli <chris.machine@pm.me>
2022-11-08 19:53:08 -0500

    feat: add basic sane config options

---
+++ supercharge.plugin.zsh
@@ -0,0 +1,53 @@
+# completions
+autoload -Uz compinit
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+_comp_options+=(globdots)		# Include hidden files.
+zle_highlight=('paste:none')
+
+unsetopt BEEP
+setopt AUTO_CD
+setopt GLOB_DOTS
+setopt NOMATCH
+setopt MENU_COMPLETE
+setopt EXTENDED_GLOB
+setopt INTERACTIVE_COMMENTS
+setopt APPEND_HISTORY
+# setopt interactive_comments
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+# Colors
+autoload -Uz colors && colors
+
+# exports
+export PATH="$HOME/.local/bin":$PATH
+
+# bindings
+bindkey -s '^x' '^usource ~/.zshrc\n'
+
+compinit
+
+# ls colors
+case "$(uname -s)" in
+
+Darwin)
+	# echo 'Mac OS X'
+	alias ls='ls -G'
+	;;
+
+Linux)
+	alias ls='ls --color=auto'
+	;;
+
+CYGWIN* | MINGW32* | MSYS* | MINGW*)
+	# echo 'MS Windows'
+	;;
+*)
+	# echo 'Other OS'
+	;;
+esac
+
