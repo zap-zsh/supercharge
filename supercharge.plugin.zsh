3c9f33cb63e755e5ae03db76b71c80c8da88a04b
Soumyaranjan Acharya <ach4825@gmail.com>
2022-11-28 01:26:21 +0530

    add improvements for history (#8)
    * add improvements for history
    
    * fix HISTFILE location
    
    * add autoload for plug
    
    * speed up compinit
    
    * fix .zcompdump
    
    * Removed blank/duplicated lines
    
    Removed blank/duplicated lines and decreased `HISTSIZE`, the user can freely chose to set it higher
    
    Co-authored-by: Marco <82162277+mamaraddio@users.noreply.github.com>

--- supercharge.plugin.zsh
+++ supercharge.plugin.zsh
@@ -1,14 +1,16 @@
+autoload -Uz plug
+
 # completions
 autoload -Uz compinit
-
 zstyle ':completion:*' menu yes select
-
-
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
-
 zmodload zsh/complist
 _comp_options+=(globdots)		# Include hidden files.
 zle_highlight=('paste:none')
+for dump in "${ZDOTDIR:-$HOME}/.zcompdump"(N.mh+24); do
+  compinit
+done
+compinit -C
 
 unsetopt BEEP
 setopt AUTO_CD
@@ -18,7 +20,22 @@ setopt MENU_COMPLETE
 setopt EXTENDED_GLOB
 setopt INTERACTIVE_COMMENTS
 setopt APPEND_HISTORY
-# setopt interactive_comments
+
+HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"
+HISTSIZE=50000
+SAVEHIST=50000
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
+setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
 
 autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
@@ -32,15 +49,11 @@ autoload -Uz colors && colors
 export PATH="$HOME/.local/bin":$PATH
 
 # bindings
-
-
+bindkey -s '^x' '^usource ${ZDOTDIR:-$HOME}/.zshrc\n'
 bindkey -M menuselect '?' history-incremental-search-forward
 bindkey -M menuselect '/' history-incremental-search-backward
 
-bindkey -s '^x' '^usource $ZDOTDIR/.zshrc\n'
-
-
-compinit
+# compinit
 
 # ls colors
 case "$(uname -s)" in
