f5b35fa0a9050250340852d406291355b3c1216b
Sadik Saifi <Sadiksaifi205@gmail.com>
2022-12-02 17:50:44 +0530

    removed extended glob

--- supercharge.plugin.zsh
+++ supercharge.plugin.zsh
@@ -13,11 +13,10 @@ done
 compinit -C
 
 unsetopt BEEP
-# setopt AUTO_CD
+setopt AUTO_CD
 setopt GLOB_DOTS
 setopt NOMATCH
 setopt MENU_COMPLETE
-setopt EXTENDED_GLOB
 setopt INTERACTIVE_COMMENTS
 setopt APPEND_HISTORY
 
